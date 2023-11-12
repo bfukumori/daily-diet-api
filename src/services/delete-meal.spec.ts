fdcbd0f5bee0424602823a8aece7181ffe32a40f
Bruno Fukumori <brunofukumori@gmail.com>
2023-11-12 14:44:24 -0300

    chore: update deps and remove .only

--- src/services/delete-meal.spec.ts
+++ src/services/delete-meal.spec.ts
@@ -10,7 +10,7 @@ let mealsRespository: InMemoryMealsRepository;
 let sut: DeleteMealService;
 let userId = '';
 
-describe.only('Delete meal service', () => {
+describe('Delete meal service', () => {
   beforeEach(async () => {
     usersRepository = new InMemoryUsersRepository();
     mealsRespository = new InMemoryMealsRepository();
